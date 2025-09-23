e57451a91b200712ced8e5e33625683c4b870b1d
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-09-22 21:23:29 -0300

    feat(side-menu): add dynamic texts, icons, and routes

--- src/app/gifs/components/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+
+interface MenuOption {
+  icon: string
+  label: string
+  subLabel: string
+  route: string
+}
 
 @Component({
   selector: 'app-side-menu-options',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './side-menu-options.component.html',
   styleUrl: './side-menu-options.component.css'
 })
 export class SideMenuOptionsComponent {
 
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    },
+
+
+  ];
 }
